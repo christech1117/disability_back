7b2eaa2eef65a2c5f6c3c2f6c2b03a09edf53a71
christech1117 <christech1117@gmail.com>
2018-06-12 18:31:15 +0800

    方案管理

--- app/Http/Resources/CompanyPlanResource.php
+++ app/Http/Resources/CompanyPlanResource.php
@@ -14,6 +14,9 @@ class CompanyPlanResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => parent::toArray($request),
+            'code' => 20000
+        ];
     }
 }
